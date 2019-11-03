6d1e95f4976cefc4180c840054e145cadb04f318
Victor Maoya <victormaoya@gmail.com>
2019-11-03 18:25:52 +0300

    completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,7 +12,7 @@ export class ProductService {
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => 
-    response.json(): <Album>);
+    <Album>response.json());
   }
 
 }
